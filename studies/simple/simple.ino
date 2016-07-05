bf8b1149ee1d6a58f9ad2a8b9c2fb7a8e7abd424
Justin Manley <manleyjster@gmail.com>
2016-07-04 21:40:55 -0700

    Add animation between two CRGB colors. Make minor bugfixes.
      * Rename RGBFract to RGB88, in accordance with FastLED naming
        conventions.
      * Fix issue with sin16 wrapping around by adding 32768 to the result
        of calling sin16 (sin16 has a different range from sin8).
      * Fix issue with jumping / random colors caused by incorrect
        initialization of the RGB88Animator when called with CRGB colors.
        Fixed by using C++11 constructor delegation syntax.

--- studies/simple/simple.ino
+++ studies/simple/simple.ino
@@ -7,15 +7,26 @@
 #define DATA_PIN 10
 #define NUM_LEDS 74
 
-class RGBFract {
+/**
+ * An RGB 88 is an RGB with 16 bits per channel; the 8 bits of CRGB color are augmented by 8
+ * "fractional" bits, which are simulated using temporal dithering.
+ */
+class RGB88 {
   public:
     accum88 red, green, blue;
 
-    RGBFract() {};
-    RGBFract(accum88 _red, accum88 _green, accum88 _blue) {
+    RGB88() {};
+
+    RGB88(accum88 _red, accum88 _green, accum88 _blue) {
         red = _red;
         green = _green;
         blue = _blue;
+    }
+
+    RGB88(CRGB color) {
+      red = color.red << 8;
+      green = color.green << 8;
+      blue = color.blue << 8;
     }
 
     CRGB dither(unsigned long t) {
@@ -32,49 +43,102 @@ class RGBFract {
        
         // No dithering necessary!
         if (fract <= 1) {
-            return channel >> 8;
+          return channel >> 8;
         } 
         
         if (t % round(256 / fract) == 0) {
-            return (channel >> 8) + 1;
+          return (channel >> 8) + 1;
         }
 
         return channel >> 8;
     }
 };
 
+/*
+ * An RGB88Animator animates smoothly between two RGB88 colors.
+ */
+class RGB88Animator {
+  public:
+    RGB88 color1, color2;
+
+    // The sign for each color is -1 or 1.
+    int redSign, greenSign, blueSign;
+
+    unsigned int redDiff, greenDiff, blueDiff;
+
+    RGB88Animator() {}
+    RGB88Animator(RGB88 _color1, RGB88 _color2) {
+      color1 = _color1;
+      color2 = _color2;
+
+      redSign = color1.red < color2.red ? 1 : -1;
+      greenSign = color1.green < color2.green ? 1 : -1;
+      blueSign = color1.blue < color2.blue ? 1 : -1;
+
+      redDiff = abs(color1.red - color2.red);
+      greenDiff = abs(color1.green - color2.green);
+      blueDiff = abs(color1.blue - color2.blue);
+    }
+
+    RGB88Animator(CRGB _color1, CRGB _color2) : RGB88Animator(RGB88(_color1), RGB88(_color2)) {}
+
+    RGB88 at(unsigned long t) {
+      unsigned short rawAmplitude = sin16(t) + 32768;
+      
+      unsigned short redAmplitude = scale16(rawAmplitude, redDiff);
+      unsigned short greenAmplitude = scale16(rawAmplitude, greenDiff);
+      unsigned short blueAmplitude = scale16(rawAmplitude, blueDiff);
+
+      return RGB88(
+        color1.red + redSign * redAmplitude,
+        color1.green + greenSign * greenAmplitude,
+        color1.blue + blueSign * blueAmplitude  
+      );
+    };
+};
+
 unsigned long counter = 0;
 
-RGBFract back;
+RGB88Animator back;
+RGB88Animator middle;
+RGB88Animator front;
 
 CRGB leds[NUM_LEDS];
 
 void setup() {
   FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
 
+  back = RGB88Animator(CRGB::Aquamarine, CRGB::Coral);
+  middle = RGB88Animator(CRGB::Red, CRGB::Indigo);
+  front = RGB88Animator(CRGB::Olive, CRGB::Purple);
+
   // Don't want FastLED's built-in dithering to interfere with our manual
   // dithering.
   FastLED.setDither(0);
-
-  Serial.begin(9600);
 }
 
 void loop() {  
-  // Update the colors
-  int amplitude = scale16(sin16(counter), 50 << 8);
-  back = RGBFract(0, 0, (40 << 8) + amplitude);
-
   // LED index.
   int i = 0;
 
   // Back layer
-  CRGB backLED = back.dither(counter);
+  CRGB backLED = back.at(counter << 4).dither(counter);
   for (i = 0; i < 16; i++) {
     leds[i] = backLED;
+  }
+
+  // Middle layer
+  CRGB middleLED = middle.at(counter << 4).dither(counter);
+  for (i = 16; i < 40; i++) {
+    leds[i] = middleLED;  
+  }
+
+  // Front layer
+  CRGB frontLED = front.at(counter << 4).dither(counter);
+  for (i = 40; i < 74; i++) {
+    leds[i] = frontLED;  
   }
 
   counter += 1;
   FastLED.show();
 }
-
-
